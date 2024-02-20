5a4d253fb2e547fea5ad52e16f03b76c846d1423
01fe22bca208 <01fe22bca208@kletech.ac.in>
2024-02-20 14:13:12 +0530

    Student info

---
+++ student.java
@@ -0,0 +1,21 @@
+public class student   {
+public String course[] = { "C#", "Applied statistics", "JAVA" };
+     public int marks[] = { 70, 100, 30 };
+    public void Display() {
+        String name = "kiran";
+        String program = "BCA";
+        String department = "BCA";
+        System.out.println("Name of the student" + name);
+        System.out.println("Program" + program);
+        System.out.println("Department" + department);
+         
+          for (int i=0;i<3;i++){
+           System.out.println("Courses:"+course[i]+"\nmarks"+marks[i]);
+          }
+    }
+    public static void main(String[] args) {
+        student s = new student();
+        s.Display();
+//s.read();
+    }
+}
